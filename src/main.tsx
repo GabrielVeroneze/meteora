268fb6eb43f4ca761b14556208066fd70f91ec8c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-15 23:26:39 -0300

    :sparkles: feat: adiciona estilos globais e de reset Sass à aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@/sass/_reset.scss'
+import '@/sass/_global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode></React.StrictMode>
