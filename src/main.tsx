983083a49306177ab384b162228d4919a23de54d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-16 23:22:27 -0300

    :sparkles: feat: adiciona componente de rotas

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import AppRoutes from '@/routes'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode></React.StrictMode>
+    <React.StrictMode>
+        <AppRoutes />
+    </React.StrictMode>
 )
