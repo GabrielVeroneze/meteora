57c4cb0de097ff178a38954c0cfb04cca50fbb9a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-16 23:19:31 -0300

    :sparkles: feat: cria página Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import Cabecalho from '@/components/Cabecalho'
+import styles from './Home.module.scss'
+
+const Home = () => {
+    return (
+        <>
+            <Cabecalho />
+        </>
+    )
+}
+
+export default Home
