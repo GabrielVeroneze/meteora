be6a692186874f3f56455a03a7b0a31921ba7721
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-19 23:42:09 -0300

    :sparkles: feat: adiciona componente Carrossel na página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import Cabecalho from '@/components/Cabecalho'
+import Carrossel from '@/components/Carrossel'
 import styles from './Home.module.scss'
 
 const Home = () => {
     return (
         <>
             <Cabecalho />
+            <Carrossel />
         </>
     )
 }
