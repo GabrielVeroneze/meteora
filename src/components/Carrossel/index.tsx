aff61f9403e5bf9b627fde1107b4e452a1ccd480
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-20 23:14:11 -0300

    :sparkles: feat: adiciona novo banner

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -1,5 +1,6 @@
 import Banner from './Banner'
 import banner1 from '@/assets/images/carrossel/banner-mobile-1.png'
+import banner2 from '@/assets/images/carrossel/banner-mobile-2.png'
 import logo from '@/assets/images/logo-clara-com-texto.png'
 import styles from './Carrossel.module.scss'
 
@@ -8,6 +9,10 @@ const Carrossel = () => {
         <section className={styles.carrossel}>
             <Banner imagemFundo={banner1}>
                 <img src={logo} alt="Logotipo da Meteora" />
+            </Banner>
+            <Banner imagemFundo={banner2}>
+                <h1>Coleção Atemporal</h1>
+                <p>Estilo e qualidade para durar.</p>
             </Banner>
         </section>
     )
