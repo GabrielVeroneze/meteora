a1212ec4bcd369c7095abc52a288ec96f2a8a7be
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-20 23:00:53 -0300

    :recycle: refactor: simplifica componente Banner

--- src/components/Carrossel/Banner/index.tsx
+++ src/components/Carrossel/Banner/index.tsx
@@ -1,27 +1,17 @@
 import styles from './Banner.module.scss'
 
 interface BannerProps {
-    conteudo: 'texto' | 'imagem'
-    titulo?: string
-    texto?: string
-    imagem?: string
-    alt?: string
+    children: React.ReactNode
+    imagemFundo: string
 }
 
-const Banner = ({ conteudo, titulo, texto, imagem, alt }: BannerProps) => {
+const Banner = ({ children, imagemFundo }: BannerProps) => {
     return (
-        <div className={styles.banner}>
-            {conteudo === 'texto' && (
-                <div className={styles.conteudoTexto}>
-                    <h1 className={styles.titulo}>{titulo}</h1>
-                    <p className={styles.texto}>{texto}</p>
-                </div>
-            )}
-            {conteudo === 'imagem' && (
-                <div className={styles.conteudoImagem}>
-                    <img className={styles.imagem} src={imagem} alt={alt} />
-                </div>
-            )}
+        <div
+            className={styles.banner}
+            style={{ backgroundImage: `url(${imagemFundo})` }}
+        >
+            {children}
         </div>
     )
 }
