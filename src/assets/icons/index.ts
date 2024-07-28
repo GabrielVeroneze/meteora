3b72e5dca5f9a86f6094b4cc42801b9498bdd3ad
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-27 22:43:26 -0300

    :sparkles: feat: adiciona ícones da seção de facilidades

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,9 +1,15 @@
+import iconeCubo from './cubo.svg'
 import iconeFechar from './fechar.svg'
+import iconeFlor from './flor.svg'
 import iconeMenu from './menu.svg'
+import iconeSetas from './setas.svg'
 
 const icones = {
+    cubo: iconeCubo,
     fechar: iconeFechar,
+    flor: iconeFlor,
     menu: iconeMenu,
+    setas: iconeSetas,
 }
 
 export default icones
