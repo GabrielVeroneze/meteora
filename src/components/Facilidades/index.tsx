efe5b91764fa1a46ba8c9c4c5efd5d2507239624
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-28 23:52:13 -0300

    :recycle: refactor: extrai item da lista para o componente ItemFacilidade

--- src/components/Facilidades/index.tsx
+++ src/components/Facilidades/index.tsx
@@ -1,3 +1,4 @@
+import ItemFacilidade from './ItemFacilidade'
 import icones from '@/assets/icons'
 import styles from './Facilidades.module.scss'
 
@@ -8,39 +9,24 @@ const Facilidades = () => {
                 Conheça todas as nossas facilidades
             </h3>
             <ul className={styles.lista}>
-                <li className={styles.item}>
-                    <img
-                        className={styles.icone}
-                        src={icones.cubo}
-                        alt="Um símbolo de uma forma geométrica abstrata se assemelha a um cubo estilizado com linhas conectadas."
-                    />
-                    <h4 className={styles.subtitulo}>Pague pelo pix</h4>
-                    <p className={styles.texto}>
-                        Ganhe 5% OFF em pagamentos via PIX
-                    </p>
-                </li>
-                <li className={styles.item}>
-                    <img
-                        className={styles.icone}
-                        src={icones.setas}
-                        alt="Um símbolo composto por duas setas, cada uma curvando-se ao redor da próxima, criando um loop contínuo."
-                    />
-                    <h4 className={styles.subtitulo}>Troca grátis</h4>
-                    <p className={styles.texto}>
-                        Fique livre para trocar em até 30 dias.
-                    </p>
-                </li>
-                <li className={styles.item}>
-                    <img
-                        className={styles.icone}
-                        src={icones.flor}
-                        alt="Um símbolo floral amarelo-esverdeado com oito pétalas simétricas e um centro circular."
-                    />
-                    <h4 className={styles.subtitulo}>Sustentabilidade</h4>
-                    <p className={styles.texto}>
-                        Moda responsável, que respeita o meio ambiente.
-                    </p>
-                </li>
+                <ItemFacilidade
+                    titulo="Pague pelo pix"
+                    texto="Ganhe 5% OFF em pagamentos via PIX"
+                    icone={icones.cubo}
+                    alt="Um símbolo de uma forma geométrica abstrata se assemelha a um cubo estilizado com linhas conectadas."
+                />
+                <ItemFacilidade
+                    titulo="Troca grátis"
+                    texto="Fique livre para trocar em até 30 dias."
+                    icone={icones.setas}
+                    alt="Um símbolo composto por duas setas, cada uma curvando-se ao redor da próxima, criando um loop contínuo."
+                />
+                <ItemFacilidade
+                    titulo="Sustentabilidade"
+                    texto="Moda responsável, que respeita o meio ambiente."
+                    icone={icones.flor}
+                    alt="Um símbolo floral amarelo-esverdeado com oito pétalas simétricas e um centro circular."
+                />
             </ul>
         </section>
     )
