126f957ac57459f07acae313334d7c86b744f55f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-07-30 23:26:52 -0300

    :sparkles: feat: cria componente de rodapé

---
+++ src/components/Rodape/index.tsx
@@ -0,0 +1,17 @@
+import styles from './Rodape.module.scss'
+
+const Rodape = () => {
+    return (
+        <footer className={styles.rodape}>
+            <p className={styles.texto}>
+                2023 © Desenvolvido por{' '}
+                <a href="https://github.com/GabrielVeroneze" target="_blank">
+                    Gabriel
+                </a>{' '}
+                | Projeto fictício sem fins comerciais.
+            </p>
+        </footer>
+    )
+}
+
+export default Rodape
