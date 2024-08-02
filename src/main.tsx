c3317e6a215a20e00c301cf209968c6eec6e82d8
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-01 22:19:21 -0300

    :sparkles: feat: importa estilos do Swiper

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import AppRoutes from '@/routes'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
+import 'swiper/css/bundle'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
