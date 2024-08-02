9bd4e50cbe32dcc8f93a0372533747ec5f601af5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-01 21:11:49 -0300

    :sparkles: feat: adiciona ícones de setas direita e esquerda

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,6 +2,8 @@ import iconeCubo from './cubo.svg'
 import iconeFechar from './fechar.svg'
 import iconeFlor from './flor.svg'
 import iconeMenu from './menu.svg'
+import iconeSetaDireita from './seta-direita.svg'
+import iconeSetaEsquerda from './seta-esquerda.svg'
 import iconeSetas from './setas.svg'
 
 const icones = {
@@ -9,6 +11,8 @@ const icones = {
     fechar: iconeFechar,
     flor: iconeFlor,
     menu: iconeMenu,
+    setaDireita: iconeSetaDireita,
+    setaEsquerda: iconeSetaEsquerda,
     setas: iconeSetas,
 }
 
