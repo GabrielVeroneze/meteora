5c577ba9af4e4d57d1fc9b01240ca331b765acd5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-01 21:22:27 -0300

    :sparkles: feat: adicionar Swiper para carrossel funcional dos banners

--- src/components/Carrossel/index.tsx
+++ src/components/Carrossel/index.tsx
@@ -1,3 +1,5 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules'
 import { banners } from '@/assets/images/carrossel'
 import Banner from './Banner'
 import logo from '@/assets/images/logo-clara-com-texto.png'
@@ -6,17 +8,31 @@ import styles from './Carrossel.module.scss'
 const Carrossel = () => {
     return (
         <section className={styles.carrossel}>
-            <Banner imagem={banners.impactoPositivo}>
-                <img src={logo} alt="Logotipo da Meteora" />
-            </Banner>
-            <Banner imagem={banners.estiloQualidade}>
-                <h1>Coleção Atemporal</h1>
-                <p>Estilo e qualidade para durar.</p>
-            </Banner>
-            <Banner imagem={banners.impactoVisual}>
-                <h1>Coleção Atemporal</h1>
-                <p>Alto impacto visual, baixo impacto ambiental.</p>
-            </Banner>
+            <Swiper
+                slidesPerView={1}
+                modules={[Navigation, Pagination, A11y, Autoplay]}
+                navigation
+                pagination={{ clickable: true }}
+                autoplay={{ delay: 4000 }}
+            >
+                <SwiperSlide>
+                    <Banner imagem={banners.impactoPositivo}>
+                        <img src={logo} alt="Logotipo da Meteora" />
+                    </Banner>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Banner imagem={banners.estiloQualidade}>
+                        <h1>Coleção Atemporal</h1>
+                        <p>Estilo e qualidade para durar.</p>
+                    </Banner>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Banner imagem={banners.impactoVisual}>
+                        <h1>Coleção Atemporal</h1>
+                        <p>Alto impacto visual, baixo impacto ambiental.</p>
+                    </Banner>
+                </SwiperSlide>
+            </Swiper>
         </section>
     )
 }
