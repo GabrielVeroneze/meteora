bb1691d5cae78be90e54fb0282e86f986c5faf7a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-02 23:05:32 -0300

    :sparkles: feat: adiciona ícones do modal

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,7 @@
+import iconeCheck from './check.svg'
 import iconeCubo from './cubo.svg'
 import iconeFechar from './fechar.svg'
+import iconeFecharModal from './fechar-modal.svg'
 import iconeFlor from './flor.svg'
 import iconeMenu from './menu.svg'
 import iconeSetaDireita from './seta-direita.svg'
@@ -7,8 +9,10 @@ import iconeSetaEsquerda from './seta-esquerda.svg'
 import iconeSetas from './setas.svg'
 
 const icones = {
+    check: iconeCheck,
     cubo: iconeCubo,
     fechar: iconeFechar,
+    fecharModal: iconeFecharModal,
     flor: iconeFlor,
     menu: iconeMenu,
     setaDireita: iconeSetaDireita,
