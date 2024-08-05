5535c015c3c3409ac3d82eea94e9de1f8ad9c84f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-05 20:24:21 -0300

    :sparkles: feat: adiciona funcionalidade de modal aos produtos

--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -1,16 +1,36 @@
+import { useState } from 'react'
 import CardProduto from './CardProduto'
+import ModalProduto from '@/components/ModalProduto'
 import produtos from '@/data/produtos.json'
 import styles from './Produtos.module.scss'
 
 const Produtos = () => {
+    const [modalEstaAberto, setModalEstaAberto] = useState<boolean>(false)
+
+    const handleAbrirModal = () => {
+        setModalEstaAberto(true)
+    }
+
+    const handleFecharModal = () => {
+        setModalEstaAberto(false)
+    }
+
     return (
         <section className={styles.produtos}>
             <h3 className={styles.titulo}>Produtos que estão bombando!</h3>
             <ul className={styles.lista}>
                 {produtos.map(produto => (
-                    <CardProduto key={produto.id} {...produto} />
+                    <CardProduto
+                        key={produto.id}
+                        onVerMais={handleAbrirModal}
+                        {...produto}
+                    />
                 ))}
             </ul>
+            <ModalProduto
+                estaAberto={modalEstaAberto}
+                onFechar={handleFecharModal}
+            />
         </section>
     )
 }
