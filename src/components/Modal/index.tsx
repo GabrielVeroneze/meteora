4dcb82c9a312fcecfedec04bf88098e8c4829e3c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-05 19:37:42 -0300

    :sparkles: feat: adicionar funcionalidade de abertura/fechamento ao modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,12 +1,24 @@
+import { useEffect, useRef } from 'react'
 import styles from './Modal.module.scss'
 
 interface ModalProps {
     children: React.ReactNode
+    estaAberto: boolean
 }
 
-const Modal = ({ children }: ModalProps) => {
+const Modal = ({ children, estaAberto }: ModalProps) => {
+    const modalRef = useRef<HTMLDialogElement | null>(null)
+
+    useEffect(() => {
+        if (estaAberto) {
+            modalRef.current?.showModal()
+        } else {
+            modalRef.current?.close()
+        }
+    }, [estaAberto])
+
     return (
-        <dialog className={styles.modal}>
+        <dialog className={styles.modal} ref={modalRef}>
             {children}
         </dialog>
     )
