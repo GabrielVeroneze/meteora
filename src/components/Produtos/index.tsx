2c1ae731a4fd736eea480851fc31fdbbe9b82345
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-08 23:42:57 -0300

    :recycle: refactor: remove lógica do modal de produto do componente Produtos

--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -1,23 +1,9 @@
-import { useState } from 'react'
-import { IProduto } from '@/types/IProduto'
 import CardProduto from './CardProduto'
 import ModalProduto from '@/components/ModalProduto'
 import produtos from '@/data/produtos.json'
 import styles from './Produtos.module.scss'
 
 const Produtos = () => {
-    const [modalEstaAberto, setModalEstaAberto] = useState<boolean>(false)
-    const [produtoSelecionado, setProdutoSelecionado] = useState<IProduto | null>(null)
-
-    const handleAbrirModal = (produto: IProduto) => {
-        setModalEstaAberto(true)
-        setProdutoSelecionado(produto)
-    }
-
-    const handleFecharModal = () => {
-        setModalEstaAberto(false)
-    }
-
     return (
         <section className={styles.produtos}>
             <h3 className={styles.titulo}>Produtos que estão bombando!</h3>
@@ -26,15 +12,10 @@ const Produtos = () => {
                     <CardProduto
                         key={produto.id}
                         produto={produto}
-                        onVerMais={handleAbrirModal}
                     />
                 ))}
             </ul>
-            <ModalProduto
-                produto={produtoSelecionado}
-                estaAberto={modalEstaAberto}
-                onFechar={handleFecharModal}
-            />
+            <ModalProduto />
         </section>
     )
 }
