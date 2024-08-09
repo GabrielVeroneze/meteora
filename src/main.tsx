f5c1021b067faf61a62d0d6c3f32791bd01702ee
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-08 23:39:14 -0300

    :sparkles: feat: envolve AppRoutes com provedores de contexto

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,15 @@ import AppRoutes from '@/routes'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
 import 'swiper/css/bundle'
+import { ModalProdutoProvider } from '@/context/modalProduto/ModalProdutoProvider'
+import { ModalNewsletterProvider } from '@/context/modalNewsletter/ModalNewsletterProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <AppRoutes />
+        <ModalProdutoProvider>
+            <ModalNewsletterProvider>
+                <AppRoutes />
+            </ModalNewsletterProvider>
+        </ModalProdutoProvider>
     </React.StrictMode>
 )
