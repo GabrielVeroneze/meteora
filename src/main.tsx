ef783b568eb45416013b7feb5987f8b84d27a33f
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-09 16:52:46 -0300

    :recycle: refactor: substitui provedores individuais pelo ContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRoutes from '@/routes'
+import { ContextProvider } from '@/context/provider/ContextsProvider'
 import '@/sass/_reset.scss'
 import '@/sass/_global.scss'
 import 'swiper/css/bundle'
-import { ModalProdutoProvider } from '@/context/modalProduto/ModalProdutoProvider'
-import { ModalNewsletterProvider } from '@/context/modalNewsletter/ModalNewsletterProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ModalProdutoProvider>
-            <ModalNewsletterProvider>
-                <AppRoutes />
-            </ModalNewsletterProvider>
-        </ModalProdutoProvider>
+        <ContextProvider>
+            <AppRoutes />
+        </ContextProvider>
     </React.StrictMode>
 )
