af7e3f967fb3cfeb5f01d09f5b9312f12a0ba21a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-10 21:29:41 -0300

    :recycle: refactor: integrar hook `useObterProdutos` em Produtos

--- src/components/Produtos/index.tsx
+++ src/components/Produtos/index.tsx
@@ -1,18 +1,17 @@
+import { useObterProdutos } from '@/hooks/useObterProdutos'
 import CardProduto from './CardProduto'
 import ModalProduto from '@/components/ModalProduto'
-import produtos from '@/data/produtos.json'
 import styles from './Produtos.module.scss'
 
 const Produtos = () => {
+    const { produtos } = useObterProdutos()
+
     return (
         <section className={styles.produtos}>
             <h3 className={styles.titulo}>Produtos que estão bombando!</h3>
             <ul className={styles.lista}>
                 {produtos.map(produto => (
-                    <CardProduto
-                        key={produto.id}
-                        produto={produto}
-                    />
+                    <CardProduto key={produto.id} produto={produto} />
                 ))}
             </ul>
             <ModalProduto />
