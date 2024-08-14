8ee09edf46b62f0d1a050020c26094fb904e25a5
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-13 21:35:00 -0300

    :sparkles: feat: adiciona controle de estado ao campo de busca

--- src/components/CampoBusca/index.tsx
+++ src/components/CampoBusca/index.tsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import styles from './CampoBusca.module.scss'
 
 const CampoBusca = () => {
+    const [termoDeBusca, setTermoDeBusca] = useState<string>('')
+
     return (
         <div className={styles.campo}>
             <input
                 className={styles.entrada}
                 type="search"
                 placeholder="Digite o produto"
+                value={termoDeBusca}
+                onChange={(evento) => setTermoDeBusca(evento.target.value)}
             />
             <button className={styles.botao}>Buscar</button>
         </div>
