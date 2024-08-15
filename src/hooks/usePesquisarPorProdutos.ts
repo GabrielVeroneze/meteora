4d3d038bb8e3afba9f20cacd345750ab9bc530c0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-14 23:36:20 -0300

    :sparkles: feat: cria hook para pesquisa de produtos

---
+++ src/hooks/usePesquisarPorProdutos.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { jsonServerApi } from '@/services/api'
+import { IProduto } from '@/types/IProduto'
+
+export const usePesquisarPorProdutos = () => {
+    const [produtos, setProdutos] = useState<IProduto[]>([])
+    const location = useLocation()
+
+    const queryParams = new URLSearchParams(location.search)
+    const query = queryParams.get('query')
+
+    useEffect(() => {
+        if (query) {
+            pesquisarProdutos(query)
+        }
+    }, [query])
+
+    const pesquisarProdutos = (query: string) => {
+        jsonServerApi
+            .get<IProduto[]>('produtos', {
+                params: {
+                    nome: query,
+                },
+            })
+            .then(resposta => {
+                setProdutos(resposta.data)
+            })
+    }
+
+    return {
+        produtos,
+    }
+}
