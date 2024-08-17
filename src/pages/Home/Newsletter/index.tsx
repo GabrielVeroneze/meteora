68d847a67f4fa067fd423e216f0212f3a43cb6cb
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-16 22:46:17 -0300

    :sparkles: feat: integra o hook `useNewsletterForm` no componente `Newsletter`

--- src/pages/Home/Newsletter/index.tsx
+++ src/pages/Home/Newsletter/index.tsx
@@ -1,21 +1,28 @@
+import { useNewsletterForm } from '@/hooks/useNewsletterForm'
 import Botao from '@/components/Botao'
 import styles from './Newsletter.module.scss'
 
 const Newsletter = () => {
+    const { register, handleSubmit, cadastrar } = useNewsletterForm()
+
     return (
         <section className={styles.secao}>
             <p className={styles.texto}>
                 Quer receber nossas novidades, promoções exclusivas e 10% OFF na
                 primeira compra? Cadastre-se!
             </p>
-            <div className={styles.campo}>
+            <form
+                className={styles.campo}
+                onSubmit={handleSubmit(cadastrar)}
+            >
                 <input
                     className={styles.entrada}
                     type="email"
                     placeholder="Digite seu email"
+                    {...register('email')}
                 />
                 <Botao>Enviar</Botao>
-            </div>
+            </form>
         </section>
     )
 }
