05c40fc6ebb135e0d121d1ceb2f006ab763355ca
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-08-19 21:58:35 -0300

    :sparkles: feat: adiciona abertura de modal após cadastro

--- src/hooks/useNewsletterForm.ts
+++ src/hooks/useNewsletterForm.ts
@@ -1,8 +1,11 @@
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { newsletterSchema, NewsletterSchemaType } from '@/schemas/newsletterSchema'
+import { useModalNewsletter } from '@/context/modalNewsletter/useModalNewsletter'
 
 export const useNewsletterForm = () => {
+    const { abrirModal } = useModalNewsletter()
+
     const {
         register,
         handleSubmit,
@@ -14,6 +17,7 @@ export const useNewsletterForm = () => {
 
     const cadastrar = (dados: NewsletterSchemaType) => {
         console.log(dados)
+        abrirModal()
     }
 
     return {
