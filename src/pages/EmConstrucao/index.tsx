cac3860f0b012f11a793e746520ea5fcb17a6717
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-09-03 23:46:31 -0300

    :sparkles: feat: cria página em construção

---
+++ src/pages/EmConstrucao/index.tsx
@@ -0,0 +1,16 @@
+import styles from './EmConstrucao.module.scss'
+
+const EmConstrucao = () => {
+    return (
+        <main className={styles.principal}>
+            <h1 className={styles.titulo}>Página em Construção</h1>
+            <p className={styles.texto}>
+                Estamos trabalhando para trazer esta página para você o mais
+                rápido possível. Enquanto isso, sinta-se à vontade para explorar
+                as outras seções do nosso site.
+            </p>
+        </main>
+    )
+}
+
+export default EmConstrucao
