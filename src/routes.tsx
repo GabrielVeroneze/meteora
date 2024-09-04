80fd951b0b7a78fe698c038bd94fca6e99452140
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-09-03 23:50:15 -0300

    :sparkles: feat: cria novas rotas com componente de página em construção

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import Layout from '@/pages/Layout'
 import Home from '@/pages/Home'
 import ProdutosCategoria from '@/pages/ProdutosCategoria'
 import ResultadosPesquisa from '@/pages/ResultadosPesquisa'
+import EmConstrucao from '@/pages/EmConstrucao'
 
 const AppRoutes = () => {
     return (
@@ -12,6 +13,9 @@ const AppRoutes = () => {
                     <Route index element={<Home />} />
                     <Route path="produtos/:categoria" element={<ProdutosCategoria />} />
                     <Route path="search" element={<ResultadosPesquisa />} />
+                    <Route path="lojas" element={<EmConstrucao />} />
+                    <Route path="novidades" element={<EmConstrucao />} />
+                    <Route path="promocoes" element={<EmConstrucao />} />
                 </Route>
             </Routes>
         </Router>
