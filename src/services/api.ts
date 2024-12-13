0b8c6d72c28d2670af5e81f1d27f34f6d87b2345
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2024-12-12 23:05:25 -0300

    ♻️ refactor: renomeia e altera exportação da instância Axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-export const jsonServerApi = axios.create({
+const api = axios.create({
     baseURL: 'http://localhost:3000/',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
     },
 })
+
+export default api
